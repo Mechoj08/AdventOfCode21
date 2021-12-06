5a965f2600f5687175d2c34731546a9878381d0a
Jochem Hoogendijk <jojochem@live.nl>
2021-12-06 09:56:46 +0100

    Changed approach for puzzle 2

--- Day6/day_6_2.py
+++ Day6/day_6_2.py
@@ -2,32 +2,28 @@ import math
 
 def parse_input():
     rates = open('Day6/puzzle_input.txt').readline()
-    #First index of tuple is fish label, second is the age
-    population = {}
-    for i, rate in enumerate(rates.split(',')):
-        population[i] = int(rate)
-    return population
+    population = [int(rate) for rate in rates.split(',')]
+    counts = [0]*9
+    for i in population:
+        counts[i] += 1
+    return counts
 
-def reproduce_gen(fish_dict):
-    new_fish_dict = {}
-    for fish in fish_dict:
-        if fish_dict[fish] == 0:
-            fish_dict[fish] = 6
-            new_fish_dict[fish] = 8
-        else:
-            fish_dict[fish] -= 1
-    for i, new_fish in enumerate(new_fish_dict):
-        fish_dict[len(fish_dict)+i] = 8
-    return fish_dict
+def reproduce_day(counts):
+    new_counts = [0]*9
+    for i, val in enumerate(counts[:-1]):
+        new_counts[i] += counts[i+1]
+    new_counts[6] += counts[0]
+    new_counts[8] += counts[0]
+    return new_counts
 
 def reproduce(days, start):
     day = 1
     fish_track = start
     while day <= days:
-        new_fish = reproduce_gen(fish_track)
+        new_fish = reproduce_day(fish_track)
         fish_track = new_fish
         day += 1
-        print(day)
-    return len(new_fish)
+        print(sum(fish_track))
+    return sum(fish_track)
 
 print(reproduce(256, parse_input()))
